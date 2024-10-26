96d13b543e1444f9dbe37fe1dcabb38a9bfb3da1
Elangovan <dharshini.elangovan@hidglobal.com>
2024-10-26 22:17:53 +0530

    changes

--- app/src/main/java/com/hid/bluetoothscannerapp/blescanner/LoginActivity.kt
+++ app/src/main/java/com/hid/bluetoothscannerapp/blescanner/LoginActivity.kt
@@ -1,7 +1,7 @@
 package com.hid.bluetoothscannerapp.blescanner
-
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebSettings
@@ -16,12 +16,12 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.gson.Gson
 import com.hid.bluetoothscannerapp.MainActivity
 import com.hid.bluetoothscannerapp.R
-import okhttp3.*
-import java.io.IOException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
-
-    private val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +42,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private fun startKeycloakLogin() {
         // Set up WebView and load the Keycloak login page
         val webView = WebView(this)
@@ -56,94 +57,64 @@ class LoginActivity : AppCompatActivity() {
         // Handle redirection and token extraction
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (url != null && url.startsWith("myapp://callback")) {
-                    // Extract JWT tokens from the redirect URL
-                    handleRedirect(Uri.parse(url))
-                    return true
+                if (url != null && url.contains("session_state") && url.contains("code")) {
+                    Log.d("TAG", "Redirect URL: $url")
+                    CoroutineScope(Dispatchers.Main).launch {
+                        try {
+                            delay(2000)
+                            webView.evaluateJavascript("(function() { return JSON.stringify(document.body.innerText); })();") { json ->
+                                try {
+                                    val jsonFormatted = json.replace("\\", "").replace("\"{", "{").replace("}\"", "}")
+                                    val gson = Gson()
+                                    val trimmedJsonString = jsonFormatted.trim('"')
+                                    val responseFromWebView = gson.fromJson(trimmedJsonString, JwtToken::class.java)
+                                    Log.d("TAG", "classGson: $responseFromWebView")
+                                    storeTokens(responseFromWebView.id_token)
+                                } catch (e: Exception) {
+                                    Log.e("TAG", "Error parsing JSON or storing token: ${e.message}", e)
+                                }
+                            }
+                        } catch (e: Exception) {
+                            Log.e("TAG", "Error executing JavaScript or coroutine scope: ${e.message}", e)
+                        }
+                    }
                 }
+
                 return false
             }
         }
     }
 
-    private fun handleRedirect(uri: Uri) {
-        // Extract the JWT token parameters from the URL
-        val idToken = uri.getQueryParameter("id_token")
-        val accessToken = uri.getQueryParameter("access_token")
-        val refreshToken = uri.getQueryParameter("refresh_token")
 
-        if (idToken != null) {
-
-            val gson = Gson()
-
-
-            val json = """
-                {
-                    "id_token": "$idToken",
-                    "access_token": "$accessToken",
-                    "refresh_token": "$refreshToken"
-                }
-            """
-
-
-            val jwtToken = gson.fromJson(json, JwtToken::class.java)
-
-
-            Log.d("LoginActivity", "Deserialized ID Token: ${jwtToken.id_token}")
-            Toast.makeText(this, "ID Token: ${jwtToken.id_token}", Toast.LENGTH_SHORT).show()
-
-
-            storeTokens(jwtToken.id_token, jwtToken.access_token, jwtToken.refresh_token)
-
-            // Make an authenticated request with OkHttp
-            makeAuthenticatedRequest(jwtToken.id_token)
-
-
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("id_token", jwtToken.id_token)
-            startActivity(intent)
-            finish()
-        } else {
-            Log.e("LoginActivity", "Error: Tokens not found in URL")
-        }
-    }
-
-    private fun storeTokens(idToken: String, accessToken: String?, refreshToken: String?) {
-        // Save tokens in SharedPreferences
-        val sharedPreferences = getSharedPreferences("auth_prefs", MODE_PRIVATE)
+    private fun storeTokens(atHash: String) {
+        val sharedPreferences = getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
-        editor.putString("id_token", idToken)
-        editor.putString("access_token", accessToken)
-        editor.putString("refresh_token", refreshToken)
+        editor.putString("id_token", atHash)
         editor.apply()
-    }
 
-    private fun makeAuthenticatedRequest(token: String) {
-        // Make an authenticated request using the token
-        val request = Request.Builder()
-            .url("http://192.168.252.124:8081/secured-endpoint")
-            .addHeader("Authorization", "Bearer $token")
-            .build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("LoginActivity", "API request failed: ${e.message}")
-            }
+        val retrievedIdToken = sharedPreferences.getString("id_token", null)
+        Log.d("StoreTokens", "Retrieved at_hash: $retrievedIdToken")
 
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    Log.d("LoginActivity", "API response: ${response.body?.string()}")
-                } else {
-                    Log.e("LoginActivity", "API request unsuccessful: ${response.message}")
-                }
-            }
-        })
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
+        // Show a toast message to confirm storage
+        Toast.makeText(this@LoginActivity, "id_token: $retrievedIdToken", Toast.LENGTH_LONG).show()
     }
 
 
     data class JwtToken(
+        val access_token: String,
+        val expires_in: Int,
+        val refresh_expires_in: Int,
+        val refresh_token: String,
+        val token_type: String,
         val id_token: String,
-        val access_token: String?,
-        val refresh_token: String?
+        val not_before_policy: Int,
+        val session_state: String,
+        val scope: String
     )
+
 }
